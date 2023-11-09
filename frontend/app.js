290a53430545bd2aeab61ca29f0d658ae772f3fb
João Beserra <joao.beserra@aluno.ifsp.edu.br>
2023-11-09 15:13:53 -0300

    tentando implentar o css na adm

--- frontend/app.js
+++ frontend/app.js
@@ -6,6 +6,7 @@ const logger = require('morgan');
 const app = express();
 
 // view engine setup
+
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.use(logger('dev'));
@@ -42,7 +43,10 @@ app.use('/scan', ScanRouterAproximar);
 
 const AdmRouterIndex = require('./routes/interface-adm/index');
 
-app.use('/adm', AdmRouterIndex)
+app.use('/adm', AdmRouterIndex);
+
+
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));

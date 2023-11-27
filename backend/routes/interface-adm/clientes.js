19f2bf37581df48bd14caaf7960d6050ccd8924c
João Beserra <joao.beserra@aluno.ifsp;edu.br>
2023-11-27 17:05:02 -0300

    adicionado colunas no client

--- backend/routes/interface-adm/clientes.js
+++ backend/routes/interface-adm/clientes.js
@@ -1,29 +1,49 @@
 var express = require("express");
 var router = express.Router();
 const { PrismaClient } = require("@prisma/client");
+const upload = require("../../middlewares/fileUpload");
 const path = require('path');
 
 
 const prisma = new PrismaClient();
 
-router.post("/cadastrar", async (req, res, next) => {
+router.post("/cadastrar", upload.single("foto"), async (req, res, next) => {
   try {
-    const nome = req.body.nome;
-    const usuario_id = Number(req.body.usuario_id);
-    const saldo = req.body.saldo;
-    const cpf = req.body.cpf;
     console.log(req.body);
-    const data = { nome, usuario_id, saldo, cpf };
-    console.log(data);
+    
+    const nome = req.body.nome;
+    const cpf = req.body.cpf;
+    const rg = req.body.rg;
+    const usuario_id = Number(req.body.usuario_id)
+    const email = req.body.email;
+    const saldo = req.body.saldo;
+    
+    const tipo = req.body.tipo;
+    const carteira = req.body.carteira;
+    const sexo = req.body.sexo;
+    const telefone = req.body.telefone;
+    
+    const foto = req.file?.path;
+    const nascimento = req.body.nascimento ? new Date(req.body.nascimento).toISOString() : null;
+
+    const data = {
+      nome, cpf, rg,
+      saldo, email,
+      tipo, carteira,
+      sexo, telefone, foto,
+      usuario_id, nascimento
+    };
+
     const cliente = await prisma.cliente.create({ data });
-    res.json({ cliente });
   } catch (error) {
     console.error(error);
     res.status(500).json({ mensagem: "Não foi possível realizar o cadastro do Cliente." });
   }
 });
 
+
 router.get("/listar", async function (req, res, next) {
+  const clientesAtivos = await prisma.cliente.count();
   const clientes = await prisma.cliente.findMany({
     orderBy: {
       id: 'desc', // Isso ordenará pelos IDs de forma decrescente, você pode usar outro campo de data se preferir
--- db/script2_adições.sql
+++ db/script2_adições.sql
@@ -14,7 +14,7 @@ add column  telefone	varchar(9);
 alter table cliente
 add column  foto        varchar(200),
 add column  rg          varchar(200),
-add column  carteira    varchar(9),
+add column  carteira    varchar(10),
 add column  cep         varchar(8),
 add column  rua         varchar(150),
 add column  bairro      varchar(150),
@@ -22,18 +22,19 @@ add column  cidade      varchar(150),
 add column  tipo        varchar(1),
 add column  sexo        varchar(1),
 add column  telefone    varchar(9),
-add column  email       varchar(100);
+add column  email       varchar(100),
+add column  nascimento  date;
 
 alter table usuario
 add column  foto        varchar(200),
 add column  cpf         varchar(11),
 add column  rg          varchar(200),
-add column  cep         varchar(8),
-add column  rua         varchar(150),
-add column  bairro      varchar(150),
-add column  cidade      varchar(150),
 add column  nacimento   date,
 add column  telefone    varchar(9);
+-- add column  cep         varchar(8),
+-- add column  rua         varchar(150),
+-- add column  bairro      varchar(150),
+-- add column  cidade      varchar(150),
 
 
 
